dd6875b0f9b83db2bdf16ef6eea267d7941335b4
Kenneth Lay <kchasel@me.com>
2014-02-10 11:48:07 -0500

    Adds missing table support with configuration option
      * Bumps version to 0.3.1

--- lib/enum_table/railtie.rb
+++ lib/enum_table/railtie.rb
@@ -1,13 +1,27 @@
 module EnumTable
   class Railtie < Rails::Railtie
+    config.enum_table = ActiveSupport::OrderedOptions.new
+
     rake_tasks do
       namespace :enum_table do
         task :load_schema_dumper do
           require 'enum_table/schema_dumper'
         end
+
+        task :allow_missing_tables do
+          EnumTable.missing_tables_allowed
+        end
       end
 
       Rake::Task['db:schema:dump'].prerequisites << 'enum_table:load_schema_dumper'
+
+      if config.enum_table.protect_tasks
+        %w'db:schema:load db:migrate db:migrate:up'.each do |task|
+          task = Rake::Task[task]
+          task.prerequisites.insert 0, 'enum_table:allow_missing_tables'
+          task.enhance { EnumRefTable.missing_tables_disallowed }
+        end
+      end
     end
   end
 end
